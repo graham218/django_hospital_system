8892eba723613d2b8b70477437c46e9e2a7c548d
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 06:59:45 +0300

    Update settings.py

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+####
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hospitalapp',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [

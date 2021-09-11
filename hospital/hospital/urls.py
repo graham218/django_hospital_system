c527714d7fe63f3c89d90573958cc6c633cdb145
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 06:04:04 +0300

    Update urls.py

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -40,4 +40,6 @@ urlpatterns = [
 
     path('afterlogin', views.afterlogin_view,name='afterlogin'),
     path('logout', LogoutView.as_view(template_name='hospital/index.html'),name='logout'),
+    
+    path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
 ]

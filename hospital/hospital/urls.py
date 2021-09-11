81b288aab9d2dd7d8d85a21739835358220fc024
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 06:12:35 +0300

    Update urls.py

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -67,4 +67,7 @@ urlpatterns = [
     path('admin-appointment', views.admin_appointment_view,name='admin-appointment'),
     path('admin-view-appointment', views.admin_view_appointment_view,name='admin-view-appointment'),
     path('admin-add-appointment', views.admin_add_appointment_view,name='admin-add-appointment'),
+    path('admin-approve-appointment', views.admin_approve_appointment_view,name='admin-approve-appointment'),
+    path('approve-appointment/<int:pk>', views.approve_appointment_view,name='approve-appointment'),
+    path('reject-appointment/<int:pk>', views.reject_appointment_view,name='reject-appointment'),
 ]

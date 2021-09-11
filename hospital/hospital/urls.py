6c48ad4092a43f82913a4125fc534c16037ec98f
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 06:00:41 +0300

    Update urls.py

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hospitalapp import views
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home_view,name=''),
+
+
+    path('aboutus', views.aboutus_view),
+    path('contactus', views.contactus_view),
 ]

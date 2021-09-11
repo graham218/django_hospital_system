135085d23d2edfc911ad2d01fe1e8445e07bd022
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 06:06:23 +0300

    Update urls.py

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -52,4 +52,7 @@ urlpatterns = [
     path('approve-doctor/<int:pk>', views.approve_doctor_view,name='approve-doctor'),
     path('reject-doctor/<int:pk>', views.reject_doctor_view,name='reject-doctor'),
     path('admin-view-doctor-specialisation',views.admin_view_doctor_specialisation_view,name='admin-view-doctor-specialisation'),
+
+    path('admin-patient', views.admin_patient_view,name='admin-patient'),
+    path('admin-view-patient', views.admin_view_patient_view,name='admin-view-patient'),
 ]

4a2b92b67dc3153bcf24c819dedc152711eb487a
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 06:16:07 +0300

    Update urls.py

--- hospital/hospital/urls.py
+++ hospital/hospital/urls.py
@@ -83,4 +83,16 @@ urlpatterns +=[
     path('doctor-view-appointment', views.doctor_view_appointment_view,name='doctor-view-appointment'),
     path('doctor-delete-appointment',views.doctor_delete_appointment_view,name='doctor-delete-appointment'),
     path('delete-appointment/<int:pk>', views.delete_appointment_view,name='delete-appointment'),
+
+]
+
+#---------FOR PATIENT RELATED URLS-------------------------------------
+urlpatterns +=[
+
+    path('patient-dashboard', views.patient_dashboard_view,name='patient-dashboard'),
+    path('patient-appointment', views.patient_appointment_view,name='patient-appointment'),
+    path('patient-book-appointment', views.patient_book_appointment_view,name='patient-book-appointment'),
+    path('patient-view-appointment', views.patient_view_appointment_view,name='patient-view-appointment'),
+    path('patient-discharge', views.patient_discharge_view,name='patient-discharge'),
+
 ]
